3824eb0fbfe748a31f86e87681e6aa801a0c5a02
Carol_Inglethe <carolinainglethe@gmail.com>
2021-09-21 13:23:38 -0300

    Updates

---
+++ precache-manifest.2c2cc8ee662ba6c9103289f1a37fe5d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d47e2ea693975aece7efe5a3735a150",
+    "url": "/ci-project-pokedex-react-router/index.html"
+  },
+  {
+    "revision": "4075f2b870091ebe521d",
+    "url": "/ci-project-pokedex-react-router/static/css/main.4f3f0241.chunk.css"
+  },
+  {
+    "revision": "7221fb8be2b28726ef8f",
+    "url": "/ci-project-pokedex-react-router/static/js/2.e3bf3614.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ci-project-pokedex-react-router/static/js/2.e3bf3614.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4075f2b870091ebe521d",
+    "url": "/ci-project-pokedex-react-router/static/js/main.11ea2434.chunk.js"
+  },
+  {
+    "revision": "f3b107f169f7ba16179f",
+    "url": "/ci-project-pokedex-react-router/static/js/runtime-main.49476ed5.js"
+  }
+]);
